7d09aa7cbb95018cea12bf21e0b153ba7331ec5d
Javier Carrillo <javier.60207@gmail.com>
2025-04-11 12:34:49 -0700

    Terminada configuracion BD

--- database/migrations/2025_04_09_195823_create_posts_table.php
+++ database/migrations/2025_04_09_195823_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('content');
+            $table->string('category');
             $table->timestamps();
         });
     }
